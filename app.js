df1a7c0cb1b555a48fb2862ba62dfbac60b8c1a1
MKukarev <misharielt@gmail.com>
2020-01-04 20:52:39 +0400

    created15

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json())
 app.listen(process.env.PORT)
 
 app.get("/", (req,res) => {
-  axios.get(`https://api.telegram.org/botfqvtg29viBxeP6RFcTprfGg`)
+  axios.get(`https://api.telegram.org/bot864912065:AAEZ6W467E4-fqvtg29viBxeP6RFcTprfGg`)
   .then(data => {
     res.send(data)
   })

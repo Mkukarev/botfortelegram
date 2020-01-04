03ebc7d9e0ca86bbe0571516a48ee56f33585fa1
MKukarev <misharielt@gmail.com>
2020-01-04 20:39:40 +0400

    created12

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
 // const axios = require("axios")
-const express = require("express")
-const bodyParser = require("body-parser")
-const app = express()
-var cors = require('cors')
+// const express = require("express")
+// const bodyParser = require("body-parser")
+// const app = express()
+// var cors = require('cors')
 
-app.use(cors())
-app.use(bodyParser.json())
-app.listen(process.env.PORT)
+// app.use(cors())
+// app.use(bodyParser.json())
+// app.listen(process.env.PORT)
 
-app.get("/", (req,res) => {
-  res.send("hello Mike")
-})
-
-/*
-
-process.env.NTBA_FIX_319 = 1;
+// app.get("/", (req,res) => {
+//   res.send("hello Mike")
+// })
 
 const TelegramBot = require('node-telegram-bot-api');
-//replace the value below with the Telegram token you receive from @BotFather
-const token = '864912065:AAEZ6W467E4-fqvtg29viBxeP6RFcTprfGg';
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = 'YOUR_TELEGRAM_BOT_TOKEN';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
@@ -46,4 +42,3 @@ bot.on('message', (msg) => {
   bot.sendMessage(chatId, 'Received your message');
 });
 
-*/

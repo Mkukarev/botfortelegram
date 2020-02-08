1266d87afbe246280c4eab7d0a97a7422d8409c6
MKukarev <misharielt@gmail.com>
2020-02-08 13:13:47 +0300

    weather and sendImgEveryDay

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ const app = express()
 const cors = require('cors')
 const token = '864912065:AAEZ6W467E4-fqvtg29viBxeP6RFcTprfGg'
 const telegramUrl = 'https://api.telegram.org/bot'
-// const myUrl = 'https://e0fc1132.ngrok.io'
-const myUrl = 'https://stark-woodland-80135.herokuapp.com'
+
+// -----  Разработка ------------------------------------------------------
+const myUrl = 'https://a0d80a2e.ngrok.io'
+let PORT = 8080
+//-------------------------------------------------------------------------
+
+
+// ------ Прод --------------------------------------------------------------
+// const myUrl = 'https://stark-woodland-80135.herokuapp.com'
+// let PORT = process.env.PORT
+//--------------------------------------------------------------------------
+
 
 const webhookRouter = require('./src/routes/webhook')
 
@@ -14,17 +24,6 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use("/webhook", webhookRouter)
 
-// let options = {
-//   webhook:{
-//     port: process.env.PORT
-//   }
-// }
-
-
-// let setWebHook = () => {
-//     axios
-//   .post(`${telegramUrl}${token}/setWebhook?url=${myUrl}webhook`)
-// }
 
 let setWebHook = () => {
   axios
@@ -33,107 +32,9 @@ let setWebHook = () => {
 
 setWebHook()
 
-const getUpdate = async () => {
-  let req = await axios.get(`https://api.telegram.org/bot864912065:AAEZ6W467E4-fqvtg29viBxeP6RFcTprfGg/getUpdates`)
-  return req.data.result
-}
-
-const sayHi = ()=>{
-  axios.get(`https://api.telegram.org/bot864912065:AAEZ6W467E4-fqvtg29viBxeP6RFcTprfGg/sendMessage?chat_id=560721174&text=hello`)
-}
-
-const sayHiInChat = ()=>{
-  axios.get(`https://api.telegram.org/bot864912065:AAEZ6W467E4-fqvtg29viBxeP6RFcTprfGg/sendMessage?chat_id=-1001477106393&text=ЗдароваБандиты!`)
-}
-// -1001477106393
-
 app.get("/", async (req, res) => {
   res.send('<h1>Здарова Бандиты!</h1>')
 })
 
-// app.post("/", async (req, res) => {
-//   console.log(req.body)
-//   res.sendStatus(200)
-// })
 
-let PORT = process.env.PORT
-// let PORT = 8080
 app.listen(PORT)
-
-
-
-
-
-
-/*
-
-process.env.NTBA_FIX_319 = 1;
-const TelegramBot = require('node-telegram-bot-api');
-
-// replace the value below with the Telegram token you receive from @BotFather
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
-
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-});
-
-*/
-
-
-
-/*
-
-process.env.NTBA_FIX_319 = 1;
-
-const TelegramBot = require('node-telegram-bot-api');
-//replace the value below with the Telegram token you receive from @BotFather
-const token = '864912065:AAEZ6W467E4-fqvtg29viBxeP6RFcTprfGg';
-
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-//
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-});
-
-*/
--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,55 +1,84 @@
 const axios = require('axios')
-const {Router} = require ('express')
+const { Router } = require('express')
 const router = Router()
+const msgUrl = 'https://api.telegram.org/bot864912065:AAEZ6W467E4-fqvtg29viBxeP6RFcTprfGg'
 
-const msgUrl = 'https://api.telegram.org/bot864912065:AAEZ6W467E4-fqvtg29viBxeP6RFcTprfGg/sendMessage?chat_id'
+let wordsBotAnswers = ["заебумба", "Заебумба", "бот", "тест", "эй"]
+let botAnswers = ['Ебаный РОАД!', 'Кто нахуй?', "не подходи, с ножа пырну", "ты че опять упал в лужу еблом?", "АЛО ВАСЬ, ЭТО ТЫ?"]
+
+let daysVideoArray = [
+    'https://acegif.com/wp-content/gifs/horoshego-ponedelnika-16.gif',
+    ,
+    'https://thumbs.gfycat.com/LamePerfumedHapuka-max-1mb.gif',
+    ,
+    'https://lifeo.ru/wp-content/uploads/gifki-pyatnica-58.gif',
+    ,
+    'https://www.tabloidxo.com/images/life/3793/1.gif'
+]
 
 
-const sayHi = ()=>{
-    axios.get(`${msgUrl}=560721174&text=hello`)
-  }
+const randomAnswers = (array) => {
+    return array[Math.floor(Math.random()*array.length)]
+}
 
-//   const sayHiInChat = ()=>{
-//     axios.get(`${msgUrl}+=-1001477106393&text=ЗдароваБандиты!`)
-//   }
-
-  const sayHiInChat = ()=>{
-    axios.get(`https://api.telegram.org/bot864912065:AAEZ6W467E4-fqvtg29viBxeP6RFcTprfGg/sendMessage?chat_id=-1001477106393&text=ЗдароваБандиты!`)
-    // axios.get(`${msgUrl}=-1001477106393&text=hello!`)
-  }
-
-router.post("/", (req, res)=>{
-    
-    if (req.body.message != undefined){
-        let text = req.body.message.text
-        let wordsBotAnswers = ["заебумба", "Заебумба", "бот", "тест", "эй"]
-        if(
-            wordsBotAnswers.some(some => some === text)
-        ){
-        // sayHiInChat()
-        // axios.get(`https://api.telegram.org/bot864912065:AAEZ6W467E4-fqvtg29viBxeP6RFcTprfGg/sendMessage?chat_id=-1001477106393&text=ЗдароваБандиты!`)
-        // .then(res.send())
-        
-        // axios.get('https://api.telegram.org/bot864912065:AAEZ6W467E4-fqvtg29viBxeP6RFcTprfGg/sendMessage?chat_id=-1001477106393&text=ЗдароваБандиты!')
-        // axios.get(`${msgUrl}=560721174&text=hello`)
-        let botAnswers = ['Ебаный РОАД!', 'Кто нахуй?', "не подходи, с ножа пырну", "ты че опять упал в лужу еблом?", "АЛО ВАСЬ, ЭТО ТЫ?"]
-        axios.get('https://api.telegram.org/bot864912065:AAEZ6W467E4-fqvtg29viBxeP6RFcTprfGg/sendMessage',{
-            params:{
-                'chat_id':'-1001477106393',
-                'text': botAnswers[Math.floor(Math.random()*botAnswers.length)]
-            }
-        })
-        // axios.get(`${msgUrl}=-1001477106393&text=che_nahu'!`)
-        .then(()=>res.send())
-        .catch((err)=>{
-            console.log(err)
-            res.send()
+const sendMessage = (message) => {
+    axios.get(`${msgUrl}/sendMessage`, {
+        params: {
+            'chat_id': '-1001477106393',
+            'text': message
         }
-        )
-        
-    }
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+const sendVideo = (video) => {
+    axios.get(`${msgUrl}/sendVideo`, {
+        params: {
+            'chat_id': '-1001477106393',
+            'video': video
+        }})
+        .catch((err) => {
+            console.log(err)
+        })
+    
+}
+
+// --------------------- Обработка обращений к боту --------------
+
+router.post("/", (req, res) => {
+    let message = req.body.message
+    if (message != undefined) {
+        let text = message.text
+        if ( wordsBotAnswers.some(some => some === text)) {sendMessage(randomAnswers(botAnswers))}
+        if (text === "погода" || text === "Погода"){sendMessage("Заебись")}
     }
     res.send()
 })
 
+// ---------------------- Вкидываем в чат гифки на каждый день --------------------------------------------
+
+let checkTime = () => {
+    let time = new Date()
+    let day = time.getDay()
+    let hour= time.getHours()
+    let minutes= time.getMinutes()
+    let seconds = time.getSeconds()
+    console.log(day, hour, minutes, seconds)
+    if (day === 1 && hour === 8 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[0])}
+    if (day === 2 && hour === 8 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[1])}
+    if (day === 3 && hour === 8 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[2])}
+    if (day === 4 && hour === 8 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[3])}
+    if (day === 5 && hour === 16 && minutes === 20 && seconds === 0){sendVideo(daysVideoArray[4])}
+    if (day === 6 && hour === 9 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[5])}
+    if (day === 7 && hour === 9 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[6])}
+}
+
+setInterval(checkTime, 1000)
+// -------------------------------------------------------------------------------------------------------
+
+
+
 module.exports = router
+

76ce5266faea918e3129d3faa063bb34d06ed0af
MKukarev <misharielt@gmail.com>
2020-02-08 13:18:49 +0300

    weather and sendImgEveryDay

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -8,11 +8,11 @@ let botAnswers = ['Ебаный РОАД!', 'Кто нахуй?', "не подх
 
 let daysVideoArray = [
     'https://acegif.com/wp-content/gifs/horoshego-ponedelnika-16.gif',
-    ,
+    'https://i.gifer.com/E1tP.gif',
     'https://thumbs.gfycat.com/LamePerfumedHapuka-max-1mb.gif',
-    ,
+    'https://i.gifer.com/1412.gif',
     'https://lifeo.ru/wp-content/uploads/gifki-pyatnica-58.gif',
-    ,
+    'https://i.gifer.com/Y2cH.gif',
     'https://www.tabloidxo.com/images/life/3793/1.gif'
 ]
 
@@ -66,13 +66,13 @@ let checkTime = () => {
     let minutes= time.getMinutes()
     let seconds = time.getSeconds()
     console.log(day, hour, minutes, seconds)
-    if (day === 1 && hour === 8 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[0])}
-    if (day === 2 && hour === 8 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[1])}
-    if (day === 3 && hour === 8 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[2])}
-    if (day === 4 && hour === 8 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[3])}
-    if (day === 5 && hour === 16 && minutes === 20 && seconds === 0){sendVideo(daysVideoArray[4])}
-    if (day === 6 && hour === 9 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[5])}
-    if (day === 7 && hour === 9 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[6])}
+    if (day === 1 && hour === 4 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[0])}
+    if (day === 2 && hour === 4 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[1])}
+    if (day === 3 && hour === 4 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[2])}
+    if (day === 4 && hour === 4 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[3])}
+    if (day === 5 && hour === 12 && minutes === 20 && seconds === 0){sendVideo(daysVideoArray[4])}
+    if (day === 6 && hour === 5 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[5])}
+    if (day === 7 && hour === 5 && minutes === 0 && seconds === 0){sendVideo(daysVideoArray[6])}
 }
 
 setInterval(checkTime, 1000)
